99225e43cab29ed3211f3700af3ea9ff3a265463
BaevVlad <vbaev@beyondaerospace.com>
2022-06-30 10:58:14 -0700

    forgot export of new client

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { RTCServer } = require('./src/RTCServer');
-const { RTCClient } = require('./src/RTCClient');
+const { RTCClient } = require('./src/RTCClient'); 
+const { RTCClient1 } = require('./src/RTCClient1');
 module.exports.RTCServer = RTCServer;
-module.exports.RTCClient = RTCClient;
+module.exports.RTCClient = RTCClient; 
+module.exports.RTCClient1 = RTCClient1;
